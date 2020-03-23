e45410fd126675654be7a4d913bf026bdc57b8d2
Georgie <georginakellis@gmail.com>
2020-03-23 12:00:44 +0800

    beginning html bootstrap and ajax call on script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+moment().format('L');
 
+//search function//
+function searchCity(cityname) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        console.log(queryURL);
+        //empty divs and ids//
+        $("#current").empty();
+       var mainDate = moment().format('L');
+
+       //create html for city info
